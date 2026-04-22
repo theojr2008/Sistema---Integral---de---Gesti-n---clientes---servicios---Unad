d0beee44f52c8473325741a534143dc5026ed8fb
theojr2008 <149326984+theojr2008@users.noreply.github.com>
2026-04-22 09:32:35 -0500

    Set up main.py to start the application
    Initialize the application with initial data set to False.

---
+++ main.py
@@ -0,0 +1,12 @@
+from aplicacion import Aplicacion
+
+
+"""
+integrantes:
+ - Theo Junior Pérez
+
+ nota: si quiere cargar datos iniciales de clientes y servicio debe asignar el valor True a datos_iniciales
+"""
+
+app = Aplicacion(datos_iniciales=False)
+app.iniciar()
